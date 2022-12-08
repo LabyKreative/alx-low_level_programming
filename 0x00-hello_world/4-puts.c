1435bd5c61ea83bf5b8c7c9b0985526414ca60db
LabyKreative <labykreative@yahoo.com>
2022-12-08 10:09:31 -0800

    Building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return (0);
+put("\"Programming is like building a multilingual puzzle");
+return (0);
 }
