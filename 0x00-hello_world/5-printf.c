80c7eb9badbd39d61b8c4e18121cb6bdb13174cb
LabyKreative <labykreative@yahoo.com>
2022-12-08 10:41:05 -0800

    Print with printf: Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out texts to stdout.
+ * Return: 0 should the code runs successfully.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
