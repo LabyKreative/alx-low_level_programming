92cf819ad67e7f20ba192717bf216e9780e7092f
LabyKreative <labykreative@yahoo.com>
2022-12-08 10:18:23 -0800

    This troubles me a little to figure out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Description: main - Prints out the texts to stdout.
+ * Return: 0 if runs successfully.
  */
 int main(void)
 {
-put("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
