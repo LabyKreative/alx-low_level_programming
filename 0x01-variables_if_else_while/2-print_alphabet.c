6fa3998a41ca90135634728922ac571925a0f93a
LabyKreative <labykreative@yahoo.com>
2022-12-09 06:27:18 -0800

    When i can't fall asleep, I play what alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * Description: main - prints alphabets in lowercase.
+ * Use only putchar function.
+ * Return: 0
+ */
+
+int main(void)
+{
+int 1 = "a";
+while (1 <= "z")
+{
+putchar(1);
+1 += 1;
+}
+putchar("\n");
+return (0);
+}
