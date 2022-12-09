a8ad551f8e193d0fbe43546d9f70e12fcc16ac18
LabyKreative <labykreative@yahoo.com>
2022-12-09 07:45:28 -0800

    Numbers of base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Description: main - A program that prints all single digit
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
