ea7db662aacef30e5a04710ec81ddb9ed2941e54
LabyKreative <labykreative@yahoo.com>
2022-12-09 14:21:34 -0800

    C is troublesome

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of two digits
+ * Description: main - program that prints all possible different combs
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
@@ -10,24 +10,24 @@
 
 int main(void)
 {
-int n1 = 48;
-int n2;
+int num1 = 48;
+int num2;
 int com = 44;
-while (n1 <= 57)
+while (num1 <= 57)
 {
-n2 = n1 + 1;
-while (n2 <= 57)
+num2 = num1 + 1;
+while (num2 <= 57)
 {
-putchar(n1);
-putchar(n2);
-if (n1 != 56 || n2 != 57)
+putchar(num1);
+putchar(num2);
+if (num1 != 56 || n2 != 57)
 {
 putchar(com);
 putchar(32);
 }
-n2 += 1;
+num2 += 1;
 }
-n1 += 1;
+num1 += 1;
 }
 putchar('\n');
 return (0);
