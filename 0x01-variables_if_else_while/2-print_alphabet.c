1d3898a5e7580e32a0ecdfa30f941b1a3abd9f34
LabyKreative <labykreative@yahoo.com>
2022-12-09 07:09:16 -0800

    No sleepless night

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,18 @@
 #include <ctype.h>
 
 /**
- * Description: main - program that prints the alphabet in lowercase.
- * You can only use the putchar function.
+ * Description: main - A program that prints the alphabet in lowercase.
+ * You can only use the putchar function
  * Return: 0
  */
 
 int main(void)
 {
-int x = 'a';
-while (x <= 'z')
+int l = 'a';
+while (l <= 'z')
 {
 putchar(l);
-x += 1;
+l += 1;
 }
 putchar('\n');
 return (0);
