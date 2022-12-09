1dc828d1076cdfd28f21d47f4b4efd50f0216fe3
LabyKreative <labykreative@yahoo.com>
2022-12-09 08:18:55 -0800

    Still numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Decription: main - A program that prints all single digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
