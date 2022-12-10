f63e834934c9f5f4501995a4afcd39b26cfd4312
LabyKreative <labykreative@yahoo.com>
2022-12-10 03:06:16 -0800

    Nice aunty betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Description: main - prints all single digit numbers of base 10 starting from
+ * main - program that prints all single digit numbers of base 10 starting from
  * You are not allowed to use any variable of type char
  * You can only use the putchar function
  * Return: 0
