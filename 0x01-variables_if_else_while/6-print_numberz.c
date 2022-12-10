445b91c662fa0840e3a957cda522b670619b6017
LabyKreative <labykreative@yahoo.com>
2022-12-10 03:24:20 -0800

    Betty is cool

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Description: main - program entry point.
+ * Description: main-program entry point.
  * Return:0 no error, non zero if error.
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = "0"; i <= "9"; n++)
+	for (n = "0"; n <= "9"; n++)
 		putchar(n);
 	putchar("\n");
 	return (0);
