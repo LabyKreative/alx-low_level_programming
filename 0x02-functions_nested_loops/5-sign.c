3b4bb7c07ac7fc34b138828b4bab1150c9caba67
LabyKreative <labykreative@yahoo.com>
2022-12-13 15:59:32 -0800

    Sign sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
+ * print_sign - Prints the sign of a number
+ * @n: The number of which the sign will be printed
  *
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
