812eaed094c27114e33ee554e9dc45a39dd970d4
LabyKreative <labykreative@yahoo.com>
2022-12-13 15:55:59 -0800

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks alphabetic character.
+ * _isalpha - Checks if a character is alphabetic.
  * @c: The character to be checked.
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
