87119620780f66b45b48560db7d2124f006718a3
LabyKreative <labykreative@yahoo.com>
2022-12-13 07:53:08 -0800

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <putchar.h>
+
+/**
+ * Description: main - A program that prints _putchar, followed by a new line.
+ * The program should return 0.
+ * Return: executed.
+ */
+
+int main(void)
+{
+int x = 0;
+char str_putchar[9] = "_putchar";
+
+while (x < 9)
+{
+_putchar(str_putchar[n];
+x += 1;
+}
+_putchar("\n");
+return (0);
+}
