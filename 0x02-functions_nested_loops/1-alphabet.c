f19b728eec31540448bc0a5f77a01ded3395dc68
LabyKreative <labykreative@yahoo.com>
2022-12-13 08:48:31 -0800

    Test3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
+
 void print_alphabet(void)
 {
 	int n;
