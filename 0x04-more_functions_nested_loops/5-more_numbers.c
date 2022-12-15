31e36bd565e35b1066b990eca8014f40a25f13f3
LabyKreative <labykreative@yahoo.com>
2022-12-15 10:02:05 -0800

    Numbers, constitute the only universal lang!

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers -  function that prints 10 times the numbers, from 0 to 14.
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14.
  *
  * Return: 0.
  */
