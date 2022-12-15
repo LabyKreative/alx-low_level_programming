2dfcd99218ac9cfc1aa3a04458130e833592d1bb
LabyKreative <labykreative@yahoo.com>
2022-12-14 20:10:11 -0800

    dont trust this

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
+	int i;
 	int n = 98;
 	int n1 = 1, n2 = 2;
 	int n3 = n1 + n2;
 
 	printf("%d, %d, ", n1, n2);
-	for (i = 3; i <= n; ++i)
+	for (i = 3; i < n; ++i)
 	{
 		printf("%d, ", n3);
 		n1 = n2;
