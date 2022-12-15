4dbabd99a1fb845668f399e466d4cb46d87e9ab0
LabyKreative <labykreative@yahoo.com>
2022-12-15 09:42:47 -0800

    Believe in numbers and signs always

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0.
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
 void print_most_numbers(void)
 {
