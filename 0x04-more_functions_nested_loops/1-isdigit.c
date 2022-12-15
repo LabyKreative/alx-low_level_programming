90c821ebe303e621aae5bc8458ab5e86bc255d52
LabyKreative <labykreative@yahoo.com>
2022-12-15 08:23:53 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,17 +7,17 @@
  *
  * Description: checks for a digit.
  *
- * Return: Always 0 (Success)
+ * Return: 1 if c is a digit, 0 otherwise.
  */
 
 int _isdigit(int c)
 {
-	if (c >= "0" && c <= "9")
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= "0" && c <= "9")
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
