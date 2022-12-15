48234561ca1ec33cb679fda2190eaf44bdcc0aa8
LabyKreative <labykreative@yahoo.com>
2022-12-15 04:09:35 -0800

    Computer class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,13 +12,13 @@ int main(void)
 	int n3 = n1 + n2;
 
 	printf("%d, %d, ", n1, n2);
-	for (i = 3; i < n; ++i)
+	for (i = 3; i <= n; ++i)
 	{
 		printf("%d, ", n3);
 		n1 = n2;
 		n2 = n3;
 		n3 = n1 + n2;
 	}
-	printf("\n");
+	putchar("\n");
 	return (0);
 }
