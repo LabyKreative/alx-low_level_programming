6378f812cc9b8255c626cf1e2a26ba152fd4f85c
LabyKreative <labykreative@yahoo.com>
2022-12-15 04:31:45 -0800

    Cumputer class is fantastic

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,21 +4,22 @@
  * main - prints 98 fibonacci terms
  * Return: 0
  */
+
 int main(void)
 {
-        int i;
-        int n = 98;
-        int n1 = 1, n2 = 2;
-        int n3 = n1 + n2;
+int i;
+int n = 98;
+int n1 = 1, n2 = 2;
+int n3 = n1 + n2;
 
-        printf("%d, %d, ", n1, n2);
-        for (i = 3; i <= n; ++i)
-        {
-                printf("%d, ", n3);
-                n1 = n2;
-                n2 = n3;
-                n3 = n1 + n2;
-        }
-        printf("\n");
-        return (0);
+printf("%d, %d, ", n1, n2);
+for (i = 3; i <= n; ++i)
+{
+printf("%d, ", n3);
+n1 = n2;
+n2 = n3;
+n3 = n1 + n2;
+}
+printf("\n");
+return (0);
 }
