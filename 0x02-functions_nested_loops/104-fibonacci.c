747a51083a7e0c5bd6e9bd522650b73adf1982f0
LabyKreative <labykreative@yahoo.com>
2022-12-16 00:49:13 -0800

    Fibonacci of 1 and 2 and 100

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 
 /**
- * main - prints 98 fibonacci terms
+ * main - prints the first 98 Fibonacci numbers
+ *
  * Return: 0
  */
-
 int main(void)
 {
-int i;
-int n = 98;
-int n1 = 1, n2 = 2;
-int n3 = n1 + n2;
+	int count;
+	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long f1_hf1, f1_hf2, f2_hf1, f2_hf2;
+	unsigned long hf1, hf2;
 
-printf("%d, %d, ", n1, n2);
-for (i = 3; i <= n; ++i)
-{
-printf("%d, ", n3);
-n1 = n2;
-n2 = n3;
-n3 = n1 + n2;
-}
-printf("\n");
-return (0);
+	for (count = 0; count < 92; count++)
+	{
+		sum = f1 + f2;
+		printf("%lu, ", sum);
+
+		f1 = f2;
+		f2 = sum;
+	}
+
+	f1_hf1 = f1 / 10000000000;
+	f2_hf1 = f2 / 10000000000;
+	f1_hf2 = f1 % 10000000000;
+	f2_hf2 = f2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		hf1 = f1_hf1 + f2_hf1;
+		hf2 = f1_hf2 + f2_hf2;
+		if (f1_hf2 + f2_hf2 > 9999999999)
+		{
+			hf1 += 1;
+			hf2 %= 10000000000;
+		}
+
+		printf("%lu%lu", hf1, hf2);
+		if (count != 98)
+			printf(", ");
+
+		f1_hf1 = f2_hf1;
+		f1_hf2 = f2_hf2;
+		f2_hf1 = hf1;
+		f2_hf2 = hf2;
+	}
+	printf("\n");
+	return (0);
 }
