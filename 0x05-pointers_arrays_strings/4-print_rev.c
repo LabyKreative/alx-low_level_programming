28ec2d65dc22c6572299a0089e756165c08763b2
LabyKreative <labykreative@yahoo.com>
2022-12-19 09:05:54 -0800

    I can only go one way.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line.
+ * @s: String to print
+ * Return: Alays 0.
+ */
+
+void print_rev(char *s)
+{
+int x = 0;
+
+while (*(s + x) != '\0')
+x = += 1;
+x -= 1;
+while (x >= 0)
+{
+_putchar(*(s + x));
+x--;
+}
