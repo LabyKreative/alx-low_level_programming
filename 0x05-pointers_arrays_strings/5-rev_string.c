3ad9ef4e6b8024f0e1ec2aa55c5b88fa1067e4ce
LabyKreative <labykreative@yahoo.com>
2022-12-19 09:17:25 -0800

    A good engineer thins in reverse and asks himself about the stylistic consequences

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: String to reverse
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int x = 0;
+	int str = 0;
+	char alp;
+
+	while (*(s + x) != '\0')
+		i += 1;
+	x -= 1;
+
+	while (str < i)
+	{
+		alp = s[x];
+		s[x] = s[str];
+		s[str] = alp;
+		str++;
+		x--;
+	}
+}
