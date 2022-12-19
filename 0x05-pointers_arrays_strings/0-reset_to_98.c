2b3ef015e0f2bab3e3e226dcf2a204007fb3bf8d
LabyKreative <labykreative@yahoo.com>
2022-12-19 08:09:04 -0800

    98 Battery Street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: pointer to an int.
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
