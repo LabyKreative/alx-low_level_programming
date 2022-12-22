396f7acda2a7644d09359e5b8b65e0212dfccd08
LabyKreative <labykreative@yahoo.com>
2022-12-22 01:54:46 -0800

    Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints and integer.
+ * Return: Always 0.
+ * @n: number
+ */
+void print_number(int n)
+{
+	unsigned int size = n;
+
+	int num = 0;
+
+	int x;
+
+	if (n < 0)
+		size = -n;
+	if (n == 0)
+		num = 1;
+
+	while (size >= 1)
+	{
+		size = size / 10;
+		num++;
+	}
+
+	for (x = 0; x < num; x++)
+	{
+
+		int pow = 1;
+
+		int y;
+
+		int nu;
+
+		for (y = 0; y < num - x - 1; y++)
+		{
+			pow = pow * 10;
+		}
+
+		nu = ((n / pow) % 10);
+		if (n < 0)
+		{
+			nu = -nu;
+			if (x == 0)
+				_putchar(45);
+		}
+		_putchar(48 + d);
+	}
+}
