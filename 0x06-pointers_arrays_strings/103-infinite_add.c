5692ba3f9b7730da7a66d8d692b1a01c3b52da91
LabyKreative <labykreative@yahoo.com>
2022-12-23 04:40:04 -0800

    It is the additiono f strangeness to beauty, it confusing tho!

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,58 +11,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int a, b, c, d, e, f;
 
-	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
-	while (n1[l1] != '\0')
-		l1++;
-	while (n2[l2] != '\0')
-		l2++;
-	if (l1 + 2 > size_r || l2 + 2 > size_r)
+	for (a = 0; n1[a]; a++)
+		;
+	for (b = 0; n2[b]; b++)
+		;
+	if (a > size_r || b > size_r)
 		return (0);
-	l1--;
-	l2--;
-	while (i <= l1 || i <= l2)
+	e = 0;
+	for (a -= 1, b -= 1, c = 0; c < size_r - 1; a--, b--, k++)
 	{
-		num1 = num2 = 0;
-		if (i <= l1)
-			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		rl++;
-	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmpl = 0;
-	while (i <= rl)
-	{
-		tmp[i] = r[rl - i];
-		tmpl++;
-		i++;
-	}
-	i = 0;
-	while (i < tmpl)
-	{
-		if (r[i] == '\0')
+		f = e;
+		if (a >= 0)
+			f += n1[a] - '0';
+		if (b >= 0)
+			f += n2[b] - '0';
+		if (a < 0 && b < 0 && f == 0)
 		{
 			break;
 		}
-		r[i] = tmp[i];
-		i++;
+		e = n / 10;
+		r[c] = f % 10 + '0';
+	}
+	r[c] = '\0';
+	if (a >= 0 || b >= 0 || m)
+		return (0);
+	for (c -= 1, d = 0; d < c; c--, d++)
+	{
+		e = r[c];
+		r[c] = r[d];
+		r[d] = e;
 	}
 	return (r);
 }
