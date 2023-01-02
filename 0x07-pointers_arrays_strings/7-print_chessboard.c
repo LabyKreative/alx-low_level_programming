016b57be2c8fe2e0525820c363496db091ba652e
LabyKreative <labykreative@yahoo.com>
2023-01-02 12:08:40 -0800

    Mental chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * print_chessboard - Print a chessboard
- * @a: Given array holding chessboard
+ * print_chessboard - function that prints a chessboard
+ * @a: size of 2D array
+ * Return: none
  */
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j;
+	int i = 0;
+	int j;
 
-	i = 0;
 	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
 			j++;
 		}
-		_putchar('\n');
 		i++;
 	}
 }
