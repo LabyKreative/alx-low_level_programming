e3e44ea760c598672da13b8467ee4a6fa4e0cf66
LabyKreative <labykreative@yahoo.com>
2023-01-03 03:19:55 -0800

    Chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int x, y;
+unsigned int x, y;
 
-	x = 0;
-	while (x < 8)
-	{
-		y = 0;
-		while (y < 8)
-		{
-			_putchar(a[x][y]);
-			y++;
-		}
-		_putchar('\n');
-		x++;
-	}
+x = 0;
+while (x < 8)
+{
+y = 0;
+while (y < 8)
+{
+_putchar(a[x][y]);
+y++;
+}
+_putchar('\n');
+x++;
+}
 }
