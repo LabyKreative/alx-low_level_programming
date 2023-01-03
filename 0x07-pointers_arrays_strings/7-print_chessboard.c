286017d87e06fedae12a4eec5fb5e634628a2ebb
LabyKreative <labykreative@yahoo.com>
2023-01-03 05:56:33 -0800

    Chess mentalness

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
 /**
- * print_chessboard - Prints a chessboard.
- * @a: The chessboard to be printed.
+ * print_chessboard - a function that prints the chessboard.
+ * @a: the chess board array pointer
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int indx1, indx2;
+	int x, y;
 
-	for (indx1 = 0; a[indx1][7]; indx1++)
+	for (y = 0; y < 8; y++)
 	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-			_putchar(a[indx1][indx2]);
-
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[y][x]);
+		}
 		_putchar('\n');
 	}
 }
