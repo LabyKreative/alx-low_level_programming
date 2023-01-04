1a1bad6c0e98c14132e41a8c04cc0a0e3398dbd1
LabyKreative <labykreative@yahoo.com>
2023-01-04 09:57:31 -0800

    ALX, your subconsciousness if looking for dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,35 +3,47 @@
 /**
  * _sqrt_recursion - a function that returns the
  * natural square root of a number.
- * @n: input number
+ * @n: enter number.
  * Return: Alway 0.
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
+	{
 		return (0);
+	}
 	if (n == 1)
+	{
 		return (1);
+	}
 	if (n < 0)
+	{
 		return (-1);
+	}
 	return (getNumSqr(n, 1));
 }
 
 /**
- * getNumSqr - function that gets de num of sqroot
- *
- * @num: input number
- * @i: Counter variable
- * Return: int
+ * getNumSqr - a function that gets the number of
+ * square qroot.
+ * @num: enter number.
+ * @i: variable
+ * Return: Always 0.
  */
 
 int getNumSqr(int num, int i)
 {
 	if (num == (i * i))
+	{
 		return (i);
+	}
 	else if (num > (i * i))
+	{
 		return (getNumSqr(num, i + 1));
+	}
 	else
+	{
 		return (-1);
+	}
 }
