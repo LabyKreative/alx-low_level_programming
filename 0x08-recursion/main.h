a40a681ac8cb6b8193d2f8b58f77234396ed0111
LabyKreative <labykreative@yahoo.com>
2023-01-04 10:00:11 -0800

    I love the subconsciousness task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int getNumSqr(int num, int i);
 int getNumPrim(int num, int i);
 
 #endif
