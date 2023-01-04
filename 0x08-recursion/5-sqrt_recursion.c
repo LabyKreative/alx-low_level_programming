1db32d302dc32ac5cfcdb0d150937aed03239e56
LabyKreative <labykreative@yahoo.com>
2023-01-04 08:47:21 -0800

    You subconscious is looking for dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - a function that returns the
+ * natural square root of a number.
+ * @n: enter no.
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (getNumSqr(n, 1));
+}
+
+/**
+ * getNumSqr - a function that get the number of sqrt.
+ * @nu: enter no.
+ * @x: no. variable.
+ * Return: Always 0.
+ */
+
+int getNumSqr(int nu, int x)
+{
+	if (nu == (x * x))
+	{
+		return (x);
+	}
+	else if (nu > (x * x))
+	{
+		return (getNumSqr(nu, x + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
