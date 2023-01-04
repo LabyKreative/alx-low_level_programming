22f7b5258bbd16332634e8d8a2dd344d7f29fcf7
LabyKreative <labykreative@yahoo.com>
2023-01-04 07:46:36 -0800

    Dreams feel real while we're in them, right Ma Jay?

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: string to return.
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++
+	else
+	{
+		return (1 + _strlen_recursion(s));
+	}
+}
