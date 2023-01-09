ec6398f17e42572cc41ee4244732ccd02840ecf8
LabyKreative <labykreative@yahoo.com>
2023-01-09 08:03:39 -0800

    I will show you how great I am for real

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,112 +1,102 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * word_count - a function that counts the number of words.
- * @str: String to check
- * Return: EXIT_SUCCESS.
- */
-int word_count(char *str)
-{
-	int ct;
-	int x;
-
-	x = ct = 0;
-	while (str[x] != '\0')
-	{
-		if (str[x] != ' ' && (str[x + 1] == ' ' || str[x + 1] == '\0'))
-		{
-			ct++;
-			x++;
-		}
-		x++;
-	}
-	return (ct);
-}
-/**
- * find_words_len - a function that finds the length words.
- * @str: check length of string in
- * @words: words.
- * Return: EXIT_SUCCESS.
- */
-int *find_words_len(char *str, int words)
-{
-	int x, word, ln;
-	int *sizes;
-
-	sizes = malloc(words * sizeof(int));
-	if (sizes == NULL)
-		return (NULL);
-	x = word = 0;
-	while (word < words)
-	{
-		if (str[x] != ' ')
-		{
-			ln = 0;
-			while (str[x] != ' ')
-			{
-				ln++;
-				x++;
-			}
-			ln++;
-			sizes[word] = ln;
-			word++;
-		}
-		x++;
-	}
-	return (sizes);
-}
-/**
- * strtow - a function that splits a string into words.
- * @str: string to splits.
+ * strtow - splits string into words
+ * @str: string
  * Return: EXIT_SUCCESS.
  */
 char **strtow(char *str)
 {
-	char **nstr;
-	int words, x, y, z, new_words, *sizes;
+	char **s;
 
-	if (str == NULL || *str == '\0')
+	int words, x = 0, filler = 0;
+
+	if (str == NULL || *str == 0)
 		return (NULL);
-	words = word_count(str);
-	sizes = malloc(words * sizeof(int));
-	if (sizes == NULL)
+	words = getWords(str);
+	if (words == 0)
 		return (NULL);
-	sizes = find_words_len(str, words);
-	nstr = malloc((words + 1) * sizeof(char *));
-	if (nstr == NULL)
+	s = malloc(sizeof(char *) * (words + 1));
+	if (s == NULL)
 		return (NULL);
-	x = y = z = 0;
-	while (x < words && str[y] != '\0')
+	while (str[x])
 	{
-		new_words = x;
-		nstr[x] = malloc(sizes[x] + sizeof(char));
-		if (nstr[x] == NULL)
+		if (str[x] != ' ')
 		{
-			for (x = x - 1; x >= 0; x--)
-				free(nstr[x--]);
-			free(nstr);
-			return (NULL);
-		}
-		while (str[y] != '\0' && x == new_words)
-		{
-			if (str[y] != ' ')
+			int size = get_size_of_first(str + x);
+
+			s[filler] = malloc(sizeof(char) * (size + 1));
+			if (s[filler] == NULL)
 			{
-				while (str[y] != '\0' && str[y] != ' ')
+				int z;
+
+				for (z = 0; z < filler; z++)
 				{
-					nstr[x][z] = str[y];
-					y++;
-					z++;
+					free(s[z]);
 				}
-				nstr[x][z] = '\0';
-				x++;
-				z = 0;
+				free(s);
+				return (NULL);
 			}
-			y++;
+			fill(s[filler], str + x, size);
+			filler++;
+			x += size - 1;
 		}
+		x++;
 	}
-	nstr[x] = NULL;
-	free(sizes);
-	return (nstr);
+	s[words] = NULL;
+	return (s);
+}
+/**
+ * getWords - get number of words in string
+ * @s: string
+ * Return: EXIT_SUCCESS.
+ */
+int getWords(char *s)
+{
+	int x = 0, words = 0;
+
+	while (s[x])
+	{
+		if (x == 0 && s[x] != ' ')
+			words++;
+
+		if (s[x] == ' ' && s[x + 1] && s[x + 1] != ' ')
+		{
+			words++;
+		}
+		x++;
+	}
+	return (words);
+}
+/**
+ * get_size_of_first - size of first word in string
+ * @s: string
+ * Return: size of word
+ */
+int get_size_of_first(char *s)
+{
+	int x = 0;
+
+	while (s[x] && s[x] != ' ')
+		x++;
+	return (x);
+}
+/**
+ * fill - fills s2 in s1
+ * @s1: string1
+ * @s2: string2
+ * @size: size of s2
+ * Return: void
+ */
+void fill(char *s1, char *s2, int size)
+{
+	int x = 0;
+
+	while (x < size)
+	{
+		s1[x] = s2[x];
+		x++;
+	}
+	s1[size] = '\0';
 }
