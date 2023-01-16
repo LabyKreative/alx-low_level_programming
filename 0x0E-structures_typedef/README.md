517bd0c952d05e8704fa3178fabd094ae37ff88f
LabyKreative <labykreative@yahoo.com>
2023-01-16 05:47:43 -0800

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+Define a new type struct dog with the following elements:.
+Write a function that initialize a variable of type struct dog.
+Write a function that prints a struct dog.
+Define a new type dog_t as a new name for the type struct dog.
+Write a function that creates a new dog.
+Write a function that frees dogs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog structure.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
