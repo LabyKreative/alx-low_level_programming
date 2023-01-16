3928ab0647fa1999f659ddaf01f6f68a8dd8aa0b
LabyKreative <labykreative@yahoo.com>
2023-01-16 06:25:08 -0800

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,73 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * *_strcpy - a function that copies the given string.
+ * @dest: where to copy string to.
+ * @src: string to copy.
+ * Return: EXIT_SUCCESS.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (*(src + x) != 0)
+	{
+		*(dest + x) = *(src + x);
+		x++;
+	}
+	*(dest + x) = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - a function that creates a new dog struct.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ * Return: EXIT_SUCCESS.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name;
+	char *new_owner;
+	int ln;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	ln = 0;
+	while (name[ln] != '\0')
+	{
+		ln++;
+	}
+	ln++;
+	new_name = malloc(ln * sizeof(*owner));
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	ln = 0;
+	while (name[ln] != '\0')
+	{
+		ln++;
+	}
+	ln++;
+	new_owner = malloc(ln * sizeof(*name));
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_name, name);
+	_strcpy(new_owner, owner);
+	(*new_dog).name = new_name;
+	(*new_dog).age = age;
+	(*new_dog).owner = new_owner;
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
