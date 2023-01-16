8080a255da1555020e2f7c7496c88fcdcf4b1fba
LabyKreative <labykreative@yahoo.com>
2023-01-16 03:59:43 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was
+ * compiled from, followed by a new line.
+ *
+ * Return: EXIT_SUCCESS.
+ */
+
+int main(__attribute__((unused)))
+{
+	printf("%s\n", __FILE__)
+		return (0);
+}
