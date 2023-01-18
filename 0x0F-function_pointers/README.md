77ac5ff262130d53f1853c01c29a4ad02f713d5d
LabyKreative <labykreative@yahoo.com>
2023-01-18 10:54:27 -0800

    What is my name

--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,4 +1,5 @@
 Write a function that prints a name.
-Write a function that executes a function given as a parameter on each element of an array.
+Write a function that executes a function given as a parameter on each 
+element of an array.
 Write a function that searches for an integer.
 Write a program that performs simple operations.
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,10 @@
-#ifndef FUNCT_H
-#define FUNCT_H
+#ifndef FUNC
+#define FUNC
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-#include <stdio.h>
-#include <stdlib.h>
 
-#endif/*FUNC_H*/
+#endif/*FUNC*/
