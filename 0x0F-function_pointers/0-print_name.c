922fde9f50ac70cd237ceb5e4be12740ae87599d
LabyKreative <labykreative@yahoo.com>
2023-01-18 10:30:27 -0800

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: the name to print.
+ * @f: pointer to funct
+ * Return: EXIT_SUCCESS.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f)
+	{
+		return;
+	}
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCT_H
+#define FUNCT_H
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif/*FUNC_H*/
