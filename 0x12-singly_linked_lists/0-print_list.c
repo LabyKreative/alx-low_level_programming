10ecff9920e52973e6cd2cac2e49481f8029b818
LabyKreative <labykreative@yahoo.com>
2023-01-26 11:53:42 -0800

    Print list of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - a function that prints all the elements of a list_t list.
  * @h: list.
- * Return: EXIT_SUCCESS.
+ * Return: the number of nodes.
  */
 
 size_t print_list(const list_t *h)
