f6aafcbdb74bc328e4583daba9befc6fbf7d5df5
LabyKreative <labykreative@yahoo.com>
2023-01-26 11:15:14 -0800

    Free a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,11 +7,44 @@
  */
 void free_list(list_t *head)
 {
-	if (head != NULL)
+	list_t *node = head;
+
+	/* free strings */
+
+	while (node)
 	{
-		if (head->next != NULL)
-			free_list(head->next);
-		free(head->str);
-		free(head);
+		free(node->str);
+		node = node->next;
 	}
+
+	/* free nodes */
+
+	while (head)
+	{
+		list_t *temp = head;
+
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * get_last_node - a function that gets the last node of lists.
+ * @head: node head.
+ * Return: address of last element.
+ */
+list_t *get_last_node(list_t *head)
+{
+	list_t *last = head;
+
+	if (!last)
+	{
+		return (0);
+	}
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+	return (last);
 }
