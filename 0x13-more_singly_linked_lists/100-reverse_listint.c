68597607d117d2224f740dc099b7139fba5a423b
LabyKreative <labykreative@yahoo.com>
2023-01-30 12:55:09 -0800

    Reverese linked list; fixed index size

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * reverse_listint - a function that reverses a
+ * listint_t linked list.
+ * @head: node head.
+ * Return: pointer to first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next;
+
+	if (!head || !*head)
+	{
+		return (*head);
+	}
+
+	prev = 0;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_listint_safe - a function that prints
+ * a listint linked list.
+ * @head: node head.
+ * Return: num of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int size = 0, x;
+	const listint_t *tmp[100];
+
+	if (!head)
+		exit(98);
+
+	while (head)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (tmp[x] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (size);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		tmp[size] = head;
+		size++;
+		head = head->next;
+	}
+	return (size);
+}
