f3e8a3d67dd18caf0e11e40861660ebd4d0a1460
LabyKreative <labykreative@yahoo.com>
2023-01-31 02:55:21 -0800

    Print (safe version)!

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,58 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - a function that prints
- * a listint linked list.
- * @head: node head.
- * Return: num of nodes in the list.
+ * _r - a function that reallocates memory for an array of pointers
+ * to the nodes in a linked list.
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ * Return: new list pointer.
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t x;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (x = 0; x < size - 1; x++)
+		newlist[x] = list[x];
+	newlist[x] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe -  a function that prints a
+ * listint_t linked list.
+ * @head: pointer to the start of the list.
+ * Return: nodes number in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int size = 0, x;
-	const listint_t *tmp[100];
+	size_t x, num = 0;
+	const listint_t **list = NULL;
 
-	if (!head)
-		exit(98);
-
-	while (head)
+	while (head != NULL)
 	{
-		for (x = 0; x < size; x++)
+		for (x = 0; x < num; x++)
 		{
-			if (tmp[x] == head)
+			if (head == list[x])
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
-				return (size);
+				free(list);
+				return (num);
 			}
 		}
+		num++;
+		list = _r(list, num, head);
 		printf("[%p] %d\n", (void *)head, head->n);
-		tmp[size] = head;
-		size++;
 		head = head->next;
 	}
-	return (size);
+	free(list);
+	return (num);
 }
