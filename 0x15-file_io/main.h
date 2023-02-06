2c9425b094cf2d5b087a697e4919b33b71b6a0cf
LabyKreative <labykreative@yahoo.com>
2023-02-06 11:22:54 -0800

    Adjust the header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,15 +18,6 @@ int _strlen(const char *s);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void print_elf(int elf);
 
 #endif /* MAIN_H */
