ce604c724288d803bf4f121b96a9807a3b71311d
LabyKreative <labykreative@yahoo.com>
2023-03-16 02:40:54 -0700

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistin - a function that prints all elements of a dlistsint_t list.
+ * @h: list
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
