0e6cc563a020c3fdc352529b1dcfcf4d69928578
LabyKreative <labykreative@yahoo.com>
2023-04-07 17:06:17 -0700

    >>> ht = {} a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,26 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-
-	if (size < 1)
-	{
-		return (NULL);
-	}
+	unsigned long int x;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
 		return (NULL);
-	}
 
-	ht->array = calloc(size, sizeof(hash_node_t *));
 	ht->size = size;
-
-	if ((ht->array) == NULL)
-	{
-		free(ht);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	}
+	for (x = 0; x < size; x++)
+		ht->array[x] = NULL;
 
 	return (ht);
 }
