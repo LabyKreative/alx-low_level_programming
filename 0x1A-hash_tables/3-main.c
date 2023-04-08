3b934850e1454958d41f903f08c9e8a61b105b5e
LabyKreative <labykreative@yahoo.com>
2023-04-07 17:23:16 -0700

    >>> ht['betty'] = 'cool' a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * main - a function that checks hash table.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "betty", "holberton");
+	return (EXIT_SUCCESS);
+}
