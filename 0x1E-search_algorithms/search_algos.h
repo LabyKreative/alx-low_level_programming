7c260a6809983f5f1f0b0a99e16bc70dd412bb53
LabyKreative <labykreative@yahoo.com>
2023-07-12 20:46:07 -0700

    Add re search prototype to the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,6 +13,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
+int rec_search(int *array, size_t size, int value);
 
 /**
  * struct listint_s - singly linked list
