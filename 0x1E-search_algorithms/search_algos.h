42150b5ea675ca36373cf8d2d29e0435e5facd2c
LabyKreative <labykreative@yahoo.com>
2023-07-12 19:08:50 -0700

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SORTING_ALGOS_H
+#define SORTING_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SORTING_ALGOS_H */
