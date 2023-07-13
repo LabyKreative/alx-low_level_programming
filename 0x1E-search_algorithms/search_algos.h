4e456030580d2db30fd43a50c6643e0f5fe5074b
LabyKreative <labykreative@yahoo.com>
2023-07-12 23:31:57 -0700

    Fix: whitespace in the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -26,9 +26,9 @@ int rec_search(int *array, size_t size, int value);
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 listint_t *jump_list(listint_t *list, size_t size, int value);
@@ -45,10 +45,10 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
